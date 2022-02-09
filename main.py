fc31e4b67dadd671e6f344e8c67370b9fac146c8
Farzad dev <farzad@datacode.app>
2022-02-10 02:15:00 +0300

    Add Comment

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ while True:
     lengthPassword = input('Please enter Password length :')
     # Check if the input is a number
     if lengthPassword.isdigit():
-
+        # List of characters to use in the password
         chars=string.ascii_letters + string.digits + '!@#$%^&*()'
+        # Generate a random password
         password=''.join(random.choice(chars) for item in range(int(lengthPassword)))
         print('Your Password : {}'.format(password))
         while True:
