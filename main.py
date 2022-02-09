835faca9b229b1337d76322172dcb9d171222474
Farzad dev <farzad@datacode.app>
2022-02-10 02:12:12 +0300

    init Mini Project

---
+++ main.py
@@ -0,0 +1,22 @@
+import string,random
+while True:
+    lengthPassword = input('Please enter Password length :')
+    # Check if the input is a number
+    if lengthPassword.isdigit():
+
+        chars=string.ascii_letters + string.digits + '!@#$%^&*()'
+        password=''.join(random.choice(chars) for item in range(int(lengthPassword)))
+        print('Your Password : {}'.format(password))
+        while True:
+            # Ask user :do you want to continue?
+            answer = input('do you want to continue? (y/n)')
+            # check answer
+            if answer.lower() == 'y':
+                break
+            elif answer.lower() == 'n':
+                print('Thank you')
+                exit()
+            else:
+                print('Please enter y or n')
+    else:
+        print('Please enter number')            
